6a9c5ac052901f95a825406f5b309a3b4ab6987c
soldidi9548 <solbi9548@gmail.com>
2026-08-04 17:45:48 +0900

    Delete arcadia/script.js

--- arcadia/script.js
+++
@@ -1,1 +0,0 @@
-const io=new IntersectionObserver((entries)=>{entries.forEach(e=>{if(e.isIntersecting){e.target.classList.add('show');io.unobserve(e.target)}})},{threshold:.12});document.querySelectorAll('.reveal').forEach(el=>io.observe(el));
