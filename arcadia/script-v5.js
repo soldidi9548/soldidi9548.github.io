b2fefcc6dadeb5bbfef181aef3e22586e1c87c4f
soldidi9548 <solbi9548@gmail.com>
2026-08-04 18:06:52 +0900

    Add files via upload

---
+++ arcadia/script-v5.js
@@ -0,0 +1,1 @@
+const io=new IntersectionObserver((entries)=>{entries.forEach(e=>{if(e.isIntersecting){e.target.classList.add('show');io.unobserve(e.target)}})},{threshold:.12});document.querySelectorAll('.reveal').forEach(el=>io.observe(el));
