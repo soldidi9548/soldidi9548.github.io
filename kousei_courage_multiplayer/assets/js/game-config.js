f8d794a3fef6a42fc382cca911bb883b3ab34670
soldidi9548 <solbi9548@gmail.com>
2026-08-09 03:34:58 +0900

    Add files via upload

---
+++ kousei_courage_multiplayer/assets/js/game-config.js
@@ -0,0 +1,1 @@
+window.KOUSEI_COURAGE_CONFIG={rootPath:'courageGame',gameTitle:'밤의 해변 캠프장',maxStage:4,adminEmail:'admin@kousei.local'};
---
+++ kousei_courage_multiplayer/assets/js/students.js
@@ -0,0 +1,13 @@
+window.KOUSEI_COURAGE_STUDENTS = [
+{id:'mio',nameJP:'道上 美桜',nameKR:'미치카미 미오',grade:1,className:'A',email:'mio@kousei.local'},
+{id:'rin',nameJP:'冬咲 凛',nameKR:'후유사키 린',grade:1,className:'B',email:'rin@kousei.local'},
+{id:'yuu',nameJP:'小川 悠',nameKR:'오가와 유우',grade:1,className:'A',email:'yuu@kousei.local'},
+{id:'tsumugi',nameJP:'紫紬',nameKR:'무라사키 츠무기',grade:2,className:'A',email:'tsumugi@kousei.local'},
+{id:'mizuki',nameJP:'はせがわ みづき',nameKR:'하세가와 미즈키',grade:2,className:'B',email:'mizuki@kousei.local'},
+{id:'mei',nameJP:'天宮 梅',nameKR:'아마미야 메이',grade:2,className:'B',email:'mei@kousei.local'},
+{id:'nagi',nameJP:'桐谷 凪',nameKR:'키리타니 나기',grade:2,className:'B',email:'nagi@kousei.local'},
+{id:'yui',nameJP:'サクラバ ユイ',nameKR:'사쿠라바 유이',grade:2,className:'C',email:'yui@kousei.local'},
+{id:'kei',nameJP:'一ノ瀬 圭',nameKR:'이치노세 케이',grade:2,className:'C',email:'kei@kousei.local'},
+{id:'kaito',nameJP:'長谷川 海斗',nameKR:'하세가와 카이토',grade:3,className:'A',email:'kaito@kousei.local'},
+{id:'shuto',nameJP:'片桐 晶人',nameKR:'카타기리 슈토',grade:3,className:'C',email:'shuto@kousei.local'}
+];
